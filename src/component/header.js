25bd87e3411adf617b365b33e1e7dd318575de6d
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-11-02 03:36:19 +0900

    [fix] react단에 맞게 style.css 변경

--- src/component/header.js
+++ src/component/header.js
@@ -2,10 +2,10 @@ import style from '../common/style.css';
 
 function Header() {
     return (
-        <div className={style.tab_box} id="main_tab">
-            <div className={style.tab} id="console_tab">
+        <div class='tab_box' id="main_tab">
+            <div class='tab' id="console_tab">
                 <img src="./img/icons/mc_favicon.svg"/>
-                <span className={style.tab_name}>Monthly_Coding.index</span>
+                <span class='tab_name'>Monthly_Coding.index</span>
                 <img src="./img/icons/circle_green.svg"/>
             </div>
         </div>
