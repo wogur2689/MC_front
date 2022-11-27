435e56447f4be75735ad993f5455fbb14cbb4e35
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-11-27 23:15:11 +0900

    [refactor] 조건 코드 개선

--- src/component/find/find.js
+++ src/component/find/find.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import style from './find.module.css';
 import a from './img/a.png';
 import b from './img/b.png';
@@ -6,23 +7,31 @@ import d from './img/d.png';
 import e from './img/e.png';
 import f from './img/f.png';
 
+/**
+ * 조건
+ */
+export const Content = ({imgSrc, children}) => {
+    return <div className={style.content}>
+        <img className={style.img} src={imgSrc} alt='아이콘'/>
+        <span className={style.text}>&nbsp;{children}</span>
+        </div>
+}
+
 function Find() {
     return (
         <div id={style.align}>
             <div>
                 <div id={style.findtext}>저희는 이런 분을 찾고 있어요!</div>
                 <div id={style.margin}>
-                    <div id={style.content}><img id={style.ab} src={a}></img> 
-                    일주일에 2시간 투자</div>
-                    <div id={style.content}><img src={b}></img> 오프라인 보다 온라인 만남</div>
-                    <div id={style.content}><img src={c}></img> 결과보다는 과정을 추구</div>
+                    <Content imgSrc={a}>일주일에 2시간 투자</Content>
+                    <Content imgSrc={b}>오프라인 보다 온라인 만남</Content>
+                    <Content imgSrc={c}>결과보다는 과정을 추구</Content>
                 </div>
                 <div id={style.margin}>
-                    <div id={style.content}><img src={d}></img> 다양한 코딩 프로젝트를 경험</div>
-                    <div id={style.content}><img src={e}></img> 여러 분야 사람과 협업</div>
-                    <div id={style.content}><img src={f}></img> 다양한 개발툴 경험</div>
-                </div>
-                
+                    <Content imgSrc={d}>다양한 코딩 프로젝트를 경험</Content>
+                    <Content imgSrc={e}>여러 분야 사람과 협업</Content>
+                    <Content imgSrc={f}>다양한 개발툴 경험</Content>
+                </div>   
             </div>
         </div>
     )
