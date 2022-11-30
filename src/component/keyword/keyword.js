448956c4cc6f7f0771722ef9d4a89cfce38e6b52
kimseowoo03 <kimseowoo03@gmail.com>
2022-11-30 22:23:13 +0900

    [feat] 모임키워드 추가

---
+++ src/component/keyword/keyword.js
@@ -0,0 +1,32 @@
+import style from "./keyword.module.css";
+
+const KEYWORD_DATA = [
+  "인원모집중",
+  "소통",
+  "게더타운",
+  "협업",
+  "주1회",
+  "결과보다는경험:)",
+  "Android",
+  "웹 프론트엔드",
+  "백엔드",
+  "ios",
+  "백엔드",
+  "리눅스",
+  "도커",
+];
+
+function Keyword() {
+  return (
+    <div className={style.keyword_container}>
+      <h2>모임 키워드</h2>
+      <div className={style.keyword_box}>
+      {KEYWORD_DATA.map((data) => {
+        return <div className={style.keyword}><p># {data}</p></div>
+      })}
+      </div>
+    </div>
+  );
+}
+
+export default Keyword;
--- src/component/main.js
+++ src/component/main.js
@@ -3,16 +3,18 @@ import Title from './title/title';
 import Classrule from './classrule/classrule';
 import Find from './find/find';
 import Support from './support/support';
+import Keyword from './keyword/keyword';
 
 function Main() {
   return (
     <div className={style.main}>
-          <Title/>
-          <Classrule/>
-          <Find/>
-          <Support/>
+        <Title />
+        <Keyword />
+        <Classrule />
+        <Find />
+        <Support />
     </div>
-  )
+  );
 }
 
 export default Main;
