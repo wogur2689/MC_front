a6dd9fb8bc08115134fe08e89cbce4c4efecdbe5
hansolj991 <66578282+hansolj991@users.noreply.github.com>
2022-12-08 23:49:31 +0900

    [feat] personal.js

--- src/component/team/personal.js
+++ src/component/team/personal.js
@@ -4,47 +4,31 @@ const INTRODUCE_DATA = [
     {
         name: "정한솔",
         job: "Android-Native",
-        stack: ["java","u"]
+        stack: ["Kotlin","Java","React","Android","Git"]
     },
-    {
-        name: "현초은",
-        job: "Backend",
-        stack: ["java","u"]
-    },
-    {
-        name: "길재혁",
-        job: "Full-Stack",
-        stack: ["java","u"]
-    },
-    {
-        name: "김서우",
-        job: "Web-Frontend",
-        stack: ["java","u"]
-    },
-    {
-        name: "문상현",
-        job: "Android-Native",
-        stack: ["java","u"]
-    },
-    {
-        name: "나강민",
-        job: "치어리더",
-        stack: ["java","u"]
-    },
-    {
-        name: "정민서",
-        job: "디자이너",
-        stack: ["java","u"]
-    },
+
 ]
 
 
 function Personal() {
     return(
-        <div className={style.introduce_box}>
-            {INTRODUCE_DATA.map((data) => {
-                return <div><p>{data.name}</p></div>
-            })}
+        <div className={style.container}>
+            <div className={style.introduce_box}>
+                <div>
+                    {INTRODUCE_DATA.map((data) => {
+                        return  <div>
+                                    <p className={style.name}>{data.name}</p>
+                                    <p className={style.major}>{data.job}</p>
+                                    <p>주요스택</p>
+                                    <div className={style.div}>
+                                        {data.stack.map((data) =>{
+                                            return  <div className={style.personal_box}><p>{data}</p></div>
+                                        })}
+                                    </div>
+                               </div>})}
+
+                </div>
+            </div>
         </div>
     );
 }
