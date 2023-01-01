fd3975ecde6ff1d8e10456ed62537540eca867a6
kimseowoo03 <kimseowoo03@gmail.com>
2023-01-02 03:17:41 +0900

    [fix] team 컴포넌트 재사용 컴포넌트로 수정

--- src/component/team/personal.js
+++ src/component/team/personal.js
@@ -6,31 +6,62 @@ const INTRODUCE_DATA = [
         job: "Android-Native",
         stack: ["Kotlin","Java","React","Android","Git"]
     },
-
+    {
+        name: "현초은",
+        job: "Backend 짜바리",
+        stack: ["Java", "Unity", "C/C#", "Python", "SpringBoot", "AWS"]
+    },
+    {
+        name: "길재혁",
+        job: "Full-Stack 개발자",
+        stack: ["JSP", "Aos", "java", "Jquery", "React", "SpringBoot"]
+    },
+    {
+        name: "문상현",
+        job: "Android-Native",
+        stack: ["Kotlin", "Git", "Java", "Firebase", "Android"]
+    },
+    {
+        name: "나강민",
+        job: "치어리더",
+        stack: ["CI|CD", "Cubernetes", "Java", "Docker", "C", "Linux", "Kotlin", "Aos"]
+    },
+    {
+        name: "김서우",
+        job: "웹 프론트엔드 취준생",
+        stack: ["JS", "React", "RTK", "Redux", "Next.js", "Typescript"]
+    },
+    {
+        name: "정민서",
+        job: "편입준비생",
+        stack: ["토익900", "Aos", "java", "Jquery", "React"]
+    },
 ]
 
 
 function Personal() {
-    return(
-        <div className={style.container}>
-            <div className={style.introduce_box}>
-                <div>
-                    {INTRODUCE_DATA.map((data) => {
-                        return  <div>
-                                    <p className={style.name}>{data.name}</p>
-                                    <p className={style.major}>{data.job}</p>
-                                    <p>주요스택</p>
-                                    <div className={style.div}>
-                                        {data.stack.map((data) =>{
-                                            return  <div className={style.personal_box}><p>{data}</p></div>
-                                        })}
-                                    </div>
-                               </div>})}
-
-                </div>
+  return (
+    <>
+      {INTRODUCE_DATA.map((data) => {
+        return (
+          <div className={style.introduce_box}>
+            <p className={style.name}>{data.name}</p>
+            <p className={style.major}>{data.job}</p>
+            <p>주요스택</p>
+            <div className={style.div}>
+              {data.stack.map((data) => {
+                return (
+                  <div className={style.personal_box}>
+                    <p>{data}</p>
+                  </div>
+                );
+              })}
             </div>
-        </div>
-    );
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Personal;
--- src/component/team/team.js
+++ src/component/team/team.js
@@ -1,23 +1,18 @@
 import Personal from "./personal";
-import Hansol from "./hansol";
-import Choeun from "./choeun";
-import Jaehyck from "./jaehyuk";
-import Kangmin from "./kangmin";
-import Sanghyun from "./sanghyun";  
-import Seowoo from "./seowoo";
-import Minseo from "./minseo";
 import style from "./team.module.css";
 
 
 function Team() {
-    return(
-        <div className={style.container}>
-            <h2>팀원 소개</h2>
-                <div className={style.introduce_box}>
-                    <Seowoo/><Choeun/><Jaehyck/><Hansol/><Sanghyun/><Kangmin/><Minseo/>
-                </div>
+  return (
+    <>
+      <h2>팀원 소개</h2>
+      <div className={style.introduce_box}>
+        <div className={style.personal_box}>
+        <Personal />
         </div>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Team;
